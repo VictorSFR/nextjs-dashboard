204c30398f147a8e58c680e5d43cf2cc4cd7f9d7
Victor <victorsfranca3@outlook.com>
2024-08-29 20:51:20 -0300

    Fetching data chapter completed

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,10 +1,11 @@
 import { sql } from '@vercel/postgres';
+
 import {
   CustomerField,
   CustomersTableType,
   InvoiceForm,
   InvoicesTable,
-  LatestInvoiceRaw,
+  LatestInvoiceRaw, 
   Revenue,
 } from './definitions';
 import { formatCurrency } from './utils';
